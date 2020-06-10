b6b3fd1a62692f0a16e83724e10037e87f24e221
run-after <run.after@protonmail.com>
2020-06-10 15:36:34 -0500

    Remove hard coded url from email templates

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,7 +3,6 @@ class UserMailer < ApplicationMailer
  
   def welcome_email(user)
     @user = user
-    @url  = 'http://localhost:3000'
     mail(to: user.email, subject: 'Welcome to Odinbook')
   end
 end
