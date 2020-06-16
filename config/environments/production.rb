4bcf71088d1ab1a414fa9ce5f1feda8d3b4ac163
run-after <run.after@protonmail.com>
2020-06-16 14:48:23 -0500

    Add SendGrid to production Trial

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,6 +71,19 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { host: 'https://heroku.com'}
 
+  # Setup the mailer config
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.smtp_settings = {
+    :user_name => ENV['SENDGRID_USERNAME'],
+    :password => ENV['SENDGRID_PASSWORD'],
+    :domain => 'https://blooming-shore-50178.herokuapp.com',
+    :address => 'smtp.sendgrid.net',
+    :port => 587,
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
+
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
